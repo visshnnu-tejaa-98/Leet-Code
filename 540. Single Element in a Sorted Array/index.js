a15ded5b025c22288f6c6d06dcc67723e5b742fe
Visshnnu Tejaa <visshnnutejaa@gmail.com>
2023-02-21 10:03:17 +0530

    540. Single Element in a Sorted Array

---
+++ 540. Single Element in a Sorted Array/index.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNonDuplicate = function (nums) {
+  for (let i = 1; i <= nums.length; i += 2) {
+    if (nums[i] !== nums[i - 1]) return nums[i - 1];
+  }
+};
