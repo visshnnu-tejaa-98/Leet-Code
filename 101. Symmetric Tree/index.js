63cd06de93b35a856a09a01defdb54c100b9d3ab
Visshnnu Tejaa <visshnnutejaa@gmail.com>
2023-03-13 10:05:25 +0530

    101. Symmetric Tree

---
+++ 101. Symmetric Tree/index.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function (root) {
+  if (!root) return true;
+  return dfs(root.left, root.right);
+
+  function dfs(leftNode, rightNode) {
+    if (!leftNode && !rightNode) {
+      return true;
+    }
+    if (
+      (leftNode && !rightNode) ||
+      (!leftNode && rightNode) ||
+      leftNode.val !== rightNode.val
+    ) {
+      return false;
+    }
+    return (
+      dfs(leftNode.right, rightNode.left) && dfs(leftNode.left, rightNode.right)
+    );
+  }
+};
