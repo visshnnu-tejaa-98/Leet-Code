145cfaff0dc3508efd1459d00d92d24c42b0343b
Visshnnu Tejaa <visshnnutejaa@gmail.com>
2023-04-17 10:01:11 +0530

    1431. Kids With the Greatest Number of Candies

---
+++ 1431. Kids With the Greatest Number of Candies/index.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+const kidsWithCandies = function (candies, extraCandies) {
+  let ans = Array(candies.length).fill(false);
+  let max = Math.max(...candies);
+  for (let i = 0; i < candies.length; i++) {
+    if (candies[i] + extraCandies >= max) ans[i] = true;
+  }
+  return ans;
+};
