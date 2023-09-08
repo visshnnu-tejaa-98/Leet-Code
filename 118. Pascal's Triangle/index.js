21bcdc395f523f406ae7446ecd283f77f8d8afdd
Visshnnu Tejaa <visshnnutejaa@gmail.com>
2023-09-08 17:21:15 +0530

    118. Pascal's Triangle

---
+++ 118. Pascal's Triangle/index.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function (numRows) {
+  let triangle = [];
+  if (numRows === 0) return triangle;
+
+  triangle.push([1]);
+
+  for (let i = 1; i < numRows; i++) {
+    let prevRow = triangle[i - 1];
+    let newRow = [1];
+
+    for (let j = 1; j < prevRow.length; j++) {
+      newRow.push(prevRow[j - 1] + prevRow[j]);
+    }
+
+    newRow.push(1);
+    triangle.push(newRow);
+  }
+
+  return triangle;
+};
